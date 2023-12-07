252c70ee6b04d9ece782e612c103db2ab226f2da
Artem <artuom-paul@mail.ru>
2023-12-07 23:00:25 +0800

    HW2

--- IntelliJ/src/main/java/OOP/seminar2/hw2/QueueBehaviour.java
+++ IntelliJ/src/main/java/OOP/seminar2/hw2/QueueBehaviour.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 public interface QueueBehaviour {
     void acceptToMarket(Actor actor);  //зашел в магазин
+
     void releaseFromMarket(List<Actor> actors);  //вышел из магазина
+
     void update();   // сделал заказ, получил заказ
+
     void marketBehaviourAdd(Actor actor); //встал в очередь
+
     void makeOrders(); //сделал заказ
+
     void takeOrders(); //получил заказ
+
     void marketBehaviourDel(); //вышел из очереди
 }
